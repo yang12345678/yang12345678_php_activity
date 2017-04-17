08ddcc3fb98d34fb9b5b83adccb6513e5ffb19cf
Robbyn Pocock <rnp11@students.uwf.edu>
2017-04-17 17:01:44 -0500

    made changes to header

--- index/index.php
+++ index/index.php
@@ -12,7 +12,7 @@
 	<div id="big_wrapper">
 
 
-	<header id="header">Gadget Inventory!</header>
+	<header id="header">Gadget Galore!</header>
 	<!-- Displaying the navigation bar -->
 	<nav id="navigation">
 		<ul>Home</ul>
