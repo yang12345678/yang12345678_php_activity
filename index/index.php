bc0ae9c66fd9f214ded116731aab750b15a533ce
Nathan Cooper <nac33@students.uwf.edu>
2018-03-17 18:29:09 +0000

    Testing

--- index/index.php
+++ index/index.php
@@ -12,7 +12,7 @@
 	<div id="big_wrapper">
 
 
-	<header id="header">Gadgets!</header>
+	<header id="header">Gadgets!!!!</header>
 	<!-- Displaying the navigation bar -->
 	<nav id="navigation">
 		<ul>Home</ul>
