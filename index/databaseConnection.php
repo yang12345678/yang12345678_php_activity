c36cf5a0bb0b57dbd74aaf41ec75c53f59bc57b1
Nathan Cooper <nac33@students.uwf.edu>
2018-04-11 01:27:15 +0000

    Testing

--- index/databaseConnection.php
+++ index/databaseConnection.php
@@ -14,7 +14,7 @@ class databaseConnection {
 	public function __construct()
 	{
 
-	        $this->dbhost = 'http://52.207.183.132/php-db';
+	        $this->dbhost = 'php-db';
 	        $this->dbname = 'Tester';
         	$this->dbuser = 'naman';
         	$this->dbpass = 'password';
