f11e1832ce1d5b0d8b2e98a85a08001b093e8d0c
Nathan Cooper <nac33@students.uwf.edu>
2018-04-15 09:06:00 -0500

    Testing

--- index/index.php
+++ index/index.php
@@ -32,11 +32,11 @@
 	else $G = $dbc->getGadget(1);
 
 ?>
-	<!-- The right side bar's data -->
+	<!-- The left side bar's data -->
     <aside id="left_side">
         <ul><b>Type </b><br/><p><?php echo $G->getName(); ?></p></ul>
-        <ul><b>Manufacturer </b><br/><p><?php echo $G->getManufacturer(); ?></p></ul>
-        <ul><b>Cost </b><br/><p><?php echo $G->getCost(); ?><p></ul>
+		<!-- Add a ul element that displays the manufacturer of the current gadget. -->
+		<!-- Add a ul element that displays the cost of the current gadget. -->
         <ul><b>Amazon </b><br/><a href="<?php $G->getLink(); ?>"><?php echo $G->getModel(); ?></a></ul>
     </aside>
 
